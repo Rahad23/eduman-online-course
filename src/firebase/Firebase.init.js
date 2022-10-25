457c822b68941b0e75c7396760f7eb12c968cb49
Rahad2u <94119962+Rahad2u@users.noreply.github.com>
2022-10-25 15:11:17 +0600

    add header

---
+++ src/firebase/Firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnKPwAMR3bgdLIySfPA9HUzuB2UH35uhE",
+  authDomain: "online-course-1f7e3.firebaseapp.com",
+  projectId: "online-course-1f7e3",
+  storageBucket: "online-course-1f7e3.appspot.com",
+  messagingSenderId: "862312809419",
+  appId: "1:862312809419:web:4724754f1cf2cd913aec4e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
